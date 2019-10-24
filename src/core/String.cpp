b4b54221f9209ceece839a996cc6652a28dd2542
mvelazc <37750462+mvelazc@users.noreply.github.com>
2019-10-24 14:51:55 -0400

    Update String.cpp

--- src/core/String.cpp
+++ src/core/String.cpp
@@ -122,11 +122,9 @@ bool String::operator==(const String &s) const {
 bool String::operator!=(const String &s) const {
 	return !(*this == s);
 }
-//The conc. operation
+	
 String String::operator+(const String &s) const {
-	//String new_string = *this;
 	String new_string;
-	//new_string._godot_string = godot::api->godot_string_operator_plus(&new_string._godot_string, &s._godot_string);
 	new_string.godot_string = godot::api->godot_string_operatorplus(&godot_string, &s._godot_string);
 
 	return new_string;
